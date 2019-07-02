5d2575ab10622609ba3f76285b6b4036a7cb615f
silverqx <silver.zachara@gmail.com>
2019-07-02 09:48:44 +0200

    added numeraljs and currency formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
 import { Link, withRouter } from 'react-router-dom'
+import numeral from 'numeral'
+import 'numeral/locales/sk'
 
 import { removeExpense } from '../actions/expenses'
+
+numeral.locale('sk')
 
 // TODO find out how to correctly pass history and expense silver
 export const ExpenseListItem = ({ dispatch, history, id, description, note, amount, createdAt}) => (
     <div>
-        <Link to={`/edit/${id}`}>{description}</Link> - {amount} - {moment(createdAt).format('LL')}
+        <Link to={`/edit/${id}`}>{description}</Link> - {numeral(amount).format('$0,0.00')} - {moment(createdAt).format('LL')}
         <button
             onClick={() => history.push(`/edit/${id}`)}
         >
